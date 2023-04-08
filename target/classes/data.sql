3256b21869682de7f81ddbc0793353f5324054bf
Valentin Tsonevski <tsonevski99@gmail.com>
2023-04-09 02:59:59 +0300

    Add files via upload

---
+++ target/classes/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO offers(id,description,image_url,name,price,weight)
+VALUES
+    (1,"beef,onion,garlic,carrot,diced tomatoes,cheese",https://www.thefoodjoy.com/wp-content/uploads/2019/07/pasta-bolognese-1.jpg,"spagetti bolognese",4.50,400),
+    (2,"tomato sauce,mozzarella cheese, and pepperoni sausage",https://riotfest.org/wp-content/uploads/2016/10/p-evid1.jpg,"pizza pepperoni",6.50,500),
+    (3,"Cocoa powder, Baking soda,Salt,butter,Brown sugar,Eggs",https://simplyhomecooked.com/wp-content/uploads/2020/05/chocolate-fudge-cookies-n-3-scaled.jpg,"double chockolate cookie",1.20,50),
+    (4,"Chicken,lettuce, tomato, onion, cheese",https://www.chicken.ca/wp-content/uploads/2020/09/healthy-chick-burgers.jpg,"chicken burger",6,300);
+
